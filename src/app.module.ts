e1d7d9f12f70ac332d42b549af89ed506ddc5a44
Shahmil Saari <shahmilsaari@gmail.com>
2026-05-13 10:52:06 +0800

    refactor: keep only users and companies modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { validateEnv } from './config/env.validation';
-import { AntiPatternsModule } from './modules/anti-patterns/anti-patterns.module';
 import { CompaniesModule } from './modules/companies/companies.module';
 import { UsersModule } from './modules/users/users.module';
 
@@ -15,7 +14,6 @@ import { UsersModule } from './modules/users/users.module';
     }),
     UsersModule,
     CompaniesModule,
-    AntiPatternsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
